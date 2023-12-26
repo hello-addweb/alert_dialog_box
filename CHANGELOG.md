2cb0248d710d38e6a63c2f81aaf8cfb70b2ff666
sultan-addweb <sultan@addwebsolution.in>
2023-12-26 13:37:41 +0530

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1
+
+- 0.0.1: initial release of alert dialog box package..
---
+++ lib/alert_dialog_box.dart
@@ -0,0 +1,20 @@
+/// Documentation
+///
+/// custom_network_image library collection.
+library alert_dialog_box;
+
+import 'package:flutter/material.dart';
+
+
+///
+/// AlertDialogBox main class..
+class AlertDialogBox extends StatelessWidget {
+  const AlertDialogBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
